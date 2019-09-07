2904b21d10a28d10d4c524c77881e4d6925b5aaa
Khoa Do <khoado.dk@gmail.com>
2019-09-07 07:10:49 -0400

    add Hero component and DefaultPage

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+import defaultBcg from '../images/defaultBcg.jpeg';
+import { Link } from 'react-router-dom';
 
-export default class Default extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello from Default</h1>
-            </div>
-        )
-    }
+export default function Default(){
+    return (
+        <>
+            <Hero img={defaultBcg} title='404' max='true'> 
+                <h2 className='text-uppercase'>page not found</h2>
+                <Link to='/' className='main-link' style={{ marginTop: '2rem' }}>
+                    return home
+                </Link>
+            </Hero>
+        </>
+    )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { ProductConsumer } from '../context';
+import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-    return (
-        <>
-            <ProductConsumer>
-                { value => { 
-                    console.log(value)
-                    return <h1>Hello from HomePage</h1>
-                }}
-            </ProductConsumer>
-        </>
-    )
+  return (
+    <Hero title="awesome gadgets" max="true">
+      <Link to="/products" className="main-link" style={{ margin: "2rem" }}>
+        Our Products
+      </Link>
+    </Hero>
+  );
 }
